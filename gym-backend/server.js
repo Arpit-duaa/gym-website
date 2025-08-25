4f90906c85b05d72da1b3dbafbe59b86574426fb
Arpit <arpit.dua789@gmail.com>
2025-08-26 04:13:14 +0530

    Structure

--- gym-backend/server.js
+++ gym-backend/server.js
@@ -8,6 +8,9 @@ const app=express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -38,13 +41,10 @@ app.post("/api/login", (req, res) => {
   });
 });
 
-app.listen(5000,()=>{
-    console.log("Server is running on http://localhost:5000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const path = require('path');
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
 
